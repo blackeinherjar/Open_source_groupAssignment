bbb16dce9b463d000ba616b96807a9ad300b55d8
blackeinherjar <hauhan1993@gmail.com>
2019-07-02 18:15:19 +0800

    Add files via upload

--- Extension 1 - blocknote-2.7.9-an+fx-windows/sidebar/change.js
+++ Extension 1 - blocknote-2.7.9-an+fx-windows/sidebar/change.js
@@ -1,8 +1,23 @@
-document.getElementById('selectcolor').addEventListener("change", function(){
 
-  var x = document.getElementById("selectcolor").value;
-  document.getElementById("contenuto_nota").setAttribute("style", "background-color:"+x);
+
+
+document.getElementById('selectfontcolor').addEventListener("change", function(){
+
+  var x = document.getElementById("selectfontcolor").value;
+  document.getElementById("contenuto_nota").setAttribute("style", "color:"+x);
+
+
+
 
 });
 
 
+document.getElementById('selectfontsize').addEventListener("change", function(){
+
+  var x = document.getElementById("selectfontsize").value;
+  document.getElementById("contenuto_nota").setAttribute("style", "font-size:"+x+"px");
+
+
+
+
+});
